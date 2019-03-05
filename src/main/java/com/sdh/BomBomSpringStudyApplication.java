9162f3a8ab6d826e2b7c59c0dbbf87ca9f006b49
sdh2398 <ehdgus2398@naver.com>
2019-03-06 01:03:12 +0900

    spring boot initialize

---
+++ src/main/java/com/sdh/BomBomSpringStudyApplication.java
@@ -0,0 +1,13 @@
+package com.sdh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BomBomSpringStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BomBomSpringStudyApplication.class, args);
+	}
+
+}
